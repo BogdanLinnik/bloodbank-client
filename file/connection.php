f84ab4d1086dfae8beb4403c932c7aac4d7c4b78
Bogdan Linnik <bogdan@geologie.com>
2025-04-22 17:03:59 +0300

    Use ssl for connection

--- file/connection.php
+++ file/connection.php
@@ -1,10 +1,13 @@
 <?php
-$servername = getenv('DB_HOST');
+$host = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, '/etc/ssl/certs/ca-certificates.crt', NULL, NULL);
+
+$conn->real_connect($host, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
